e8b8ea71093d7a0a8350fa1b8a2ea8c9505f68e2
suneel <suneel.kumar@exiverlabs.co.in>
2025-02-23 20:58:45 +0530

    bluebash-1 add env file, rds connection, create table in postgres aws, create log file

--- db_connection.py
+++ db_connection.py
@@ -1,11 +1,53 @@
 import psycopg2
+import os
+import logging
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig(filename="error_file.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a',
+                    level=logging.INFO
+                    )
+cur = None
+conn = None
+
+try:
+    conn = psycopg2.connect(
+        dbname= os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        password=os.environ.get("DB_PASSWORD")
+    )
+    
+    cur = conn.cursor()
+    logging.info("Connection successful!")
+    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+
+    conn.commit()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS items(
+        id SERIAL PRIMARY KEY,
+        content TEXT,
+        embeding VECTOR(1536)
+    )""")
+
+    conn.commit()
+except Exception as e:
+    logging.exception("exception: ", e)
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
+
+if conn is None:
+    logging.error("database not connected.")
+else:
+    logging.info("Database operations completed successfully.")
 
 
-conn = psycopg2.connect(
-    dbname="vectordb_db",
-    user="postgres",
-    host="localhost",
-    port="5432",
-    password='postgres'
-)
-cur = conn.cursor()
+
+
+
